853ad8330c4b7a7e25eb2d34edf3dbdc997520bb
Dominik <dominik@glaetzle.io>
2025-10-08 19:41:55 +0200

    added snow fall

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,13 @@
 import CardsSection from "../components/CardsSection";
 import Header from "../components/Header";
 import Footer from "../components/Footer.tsx";
+import { Snowfall } from "react-snowfall";
 
 export default function Home() {
   return (
     <>
       <Header />
+      <Snowfall />
       <CardsSection />
       <Footer />
     </>
