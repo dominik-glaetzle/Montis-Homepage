be264502040e21db89f3ff03647bc4e3bcd24a14
Dominik <dominik@glaetzle.io>
2025-10-17 12:07:09 +0200

    added vercel analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import { Outlet } from "react-router-dom";
-
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 export default function App() {
   return (
     <>
       <main>
         <Outlet />
       </main>
+      <Analytics />
+      <SpeedInsights />
     </>
   );
 }
