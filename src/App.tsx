3091ee16f78e3d34021d9897d63d92b6fe4f4403
Dominik <dominik@glaetzle.io>
2025-10-17 12:09:36 +0200

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { Outlet } from "react-router-dom";
 import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/react";
+import { Snowfall } from "react-snowfall";
 export default function App() {
   return (
     <>
       <main>
+        <Snowfall snowflakeCount={300} />
         <Outlet />
       </main>
       <Analytics />
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,11 @@
 import CardsSection from "../components/CardsSection";
 import Header from "../components/Header";
 import Footer from "../components/Footer.tsx";
-import { Snowfall } from "react-snowfall";
 
 export default function Home() {
   return (
     <>
       <Header />
-      <Snowfall snowflakeCount={300} />
       <CardsSection />
       <Footer />
     </>
